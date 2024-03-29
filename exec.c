4e42cb6642be67ed06965f1cca339a9a579d8bad
Wiraama <mawira01@students.uonbi.ac.ke>
2024-03-29 04:51:06 -0700

    all

--- exec.c
+++ exec.c
@@ -2,30 +2,25 @@
 
 /**
  * execute - execute opcode
- * @stack: stack of linked list
  * @counter: line counter
  * @file: pointer to monty file stream
  * @content: line content
  * Return: none
- * */
-int execute(char *content, stack_t **stack, unsigned int counter, FILE *file)
+ */
+int execute(char *content, unsigned int counter, FILE *file)
 {
+	stack_t **stack;
+
 	instruction_t opst[] = {
 		{"push", f_push},
 		{"pall", f_pall},
-		{"pint", f_pint},
-		{"pop", f_pop},
-		{"swap", f_swap},
-		{"add", f_add},
-		{"nop", f_nop},
-		{"sub", f_sub},
+		{"pint", f_pint}, {"pop", f_pop}, {"swap", f_swap},
+		{"add", f_add}, {"nop", f_nop}, {"sub", f_sub},
 		{"div", f_div},
 		{"mul", f_mul},
 		{"mod", f_mod},
 		{"pchar", f_pchar},
-		{"pstr", f_pstr},
-		{"rotl", f_rotl},
-		{"rotr", f_rotr},
+		{"pstr", f_pstr}, {"rotl", f_rotl}, {"rotr", f_rotr},
 		{"queue", f_queue},
 		{"stack", f_stack},
 		{NULL, NULL}
